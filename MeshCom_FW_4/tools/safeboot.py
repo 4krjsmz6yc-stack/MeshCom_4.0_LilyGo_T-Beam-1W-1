012493c3424a6f6b8121ca1752755fc1c10e858d
karamo <karahoan@drei.at>
2026-01-28 01:13:13 +0100

    new platform for safeboot

--- MeshCom_FW_4/tools/safeboot.py
+++ MeshCom_FW_4/tools/safeboot.py
@@ -27,7 +27,8 @@ def safeboot(source, target, env):
     # platformio estimates the amount of flash used to store the firmware. this
     # estimate is not accurate. we perform a final check on the firmware bin
     # size by comparing it against the respective partition size.
-    max_size = 704 * 1024  # 704KB as set in the partition table, update accordingly
+    # max_size = 704 * 1024  # 704KB as set in the partition table, update accordingly
+    max_size = 896 * 1024  # 896KB as set in the partition table, update accordingly
     fw_size = os.path.getsize(env.subst("$BUILD_DIR/${PROGNAME}.bin"))
     if fw_size > max_size:
         raise Exception("firmware binary too large: %d > %d" % (fw_size, max_size))
